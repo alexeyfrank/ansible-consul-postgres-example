70860686fdadd875e268bed755baf596c92f8298
alexeyfrank <alexeyfrank@gmail.com>
2015-08-25 16:52:08 +0300

    fixes

--- Vagrantfile
+++ Vagrantfile
@@ -9,6 +9,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.define "n1" do |n1|
     n1.vm.hostname = "n1"
     n1.vm.network "private_network", ip: "172.20.20.10"
+    n1.vm.synced_folder '.', '/vagrant'
+
 
     n1.vm.provider 'virtualbox' do |v, override|
       override.vm.box = 'ubuntu/trusty64'
@@ -27,6 +29,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.define "n2" do |n2|
     n2.vm.hostname = "n2"
     n2.vm.network "private_network", ip: "172.20.20.11"
+    n2.vm.synced_folder '.', '/vagrant'
 
     n2.vm.provider 'virtualbox' do |v, override|
       override.vm.box = 'ubuntu/trusty64'
---
+++ index.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var pg = require('pg');
+
+var conString = "postgres://username:password@postgres.service.consul/database";
+
+var client = new pg.Client(conString);
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8080, "0.0.0.0");
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8080/");

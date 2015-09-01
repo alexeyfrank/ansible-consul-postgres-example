4bcf36f73e5684ec10d46b84bca53d1d256edb0e
alexeyfrank <alexeyfrank@gmail.com>
2015-09-01 22:48:43 +0300

    fixes

--- Makefile
+++ Makefile
@@ -1,17 +1,24 @@
 master:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n1/virtualbox/private_key cm/master.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n1/virtualbox/private_key cm/master.yml
 slave1:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n2/virtualbox/private_key cm/slave.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n2/virtualbox/private_key cm/slave.yml
 slave2:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n3/virtualbox/private_key cm/slave.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n3/virtualbox/private_key cm/slave.yml
 
 
 start_postgres_master:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n1/virtualbox/private_key cm/scripts/start_postgres_master.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n1/virtualbox/private_key cm/scripts/start_postgres_master.yml
 start_postgres_slave1:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n2/virtualbox/private_key cm/scripts/start_postgres_slave.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n2/virtualbox/private_key cm/scripts/start_postgres_slave.yml
 start_postgres_slave2:
-	ansible-playbook -i cm/inventory -u vagrant --private-key=.vagrant/machines/n3/virtualbox/private_key cm/scripts/start_postgres_slave.yml -vvvv
+	ansible-playbook -i cm/inventory.ini -u vagrant --private-key=.vagrant/machines/n3/virtualbox/private_key cm/scripts/start_postgres_slave.yml
 
 deps:
 	sudo ansible-galaxy install -f -r requirements.yml
+
+start_master:
+	ansible-playbook -i cm/inventory.ini -u vagrant cm/scripts/start_postgres_master.yml --ask-pass
+start_slave:
+	ansible-playbook -i cm/inventory.ini -u vagrant cm/scripts/start_postgres_slave.yml --ask-pass
+stop_master:
+	ansible-playbook -i cm/inventory.ini -u vagrant cm/scripts/stop_master.yml --ask-pass
--- index.js
+++ index.js
@@ -3,34 +3,48 @@ var pg = require('pg');
 var dns = require('dns');
 
 var conString = "";
-var client = null;
 conString = "postgres://web_test:12345@postgres-master.service.consul/web_test";
-client = new pg.Client(conString);
 
-function query(client, callback) {
-  client.connect(function(err) {
+function query(client, query, callback) {
+  console.log("Running query: ", query);
+  client.query(query, function(err, result) {
     if(err) {
-      return console.error('could not connect to postgres', err);
+      return console.error('error running query', err);
     }
-    client.query('SELECT NOW() AS "theTime"', function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      callback(result);
-      client.end();
-    });
+    console.log("Query successfull: ", query);
+    callback(result);
   });
 }
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  query(client, function(result) {
-      console.log(result.rows[0].theTime);
-      response.write(result.rows[0].theTime + "\n");
-      response.end("Hello World\n");
-  });
-});
+function insertQuery(client, callback) {
+  var q =  "insert into roles (name, created_at, updated_at) values ('test', now(), now());";
+  query(client, q, callback);
+}
+function selectQuery(client, callback) {
+  var q = "select count(*) as count from roles;";
+  query(client, q, callback);
+}
 
-server.listen(8080, "0.0.0.0");
-console.log("Server running at http://127.0.0.1:8080/");
+setInterval(testLoop, 1000);
+
+var counter = 1;
+
+function testLoop() {
+  var localCounter = counter;
+  counter++;
+  console.log("test loop: ", localCounter);
+  pg.connect(conString, function(err, client) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    selectQuery(client, function() {
+      insertQuery(client, function() {
+        selectQuery(client, function(result) {
+          console.log("test loop finished: ", localCounter);
+          console.log("Count: ", result.rows[0].count);
+          client.end();
+        });
+      });
+    });
+  });
+}
